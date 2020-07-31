6a9be31ae54b450fd759ccbebc175238446f3e42
asifally <asifally004@gmail.com>
2020-07-31 15:14:27 -0500

    The enemies now chase the player

--- main.py
+++ main.py
@@ -21,26 +21,35 @@ class Character():
         self.x = x
         self.y = y
         self.health = health
-        self.player_img = None
+        self.character_img = None
         self.arrow_img = None
         self.arrows = []
         self.cool_down_counter = 0
 
     def draw(self, window):
-        window.blit(self.player_img, (self.x, self.y))
+        window.blit(self.character_img, (self.x, self.y))
 
     def get_width(self):
-        return self.player_img.get_width()
+        return self.character_img.get_width()
 
     def get_height(self):
-        return self.player_img.get_height()
+        return self.character_img.get_height()
+class Enemy(Character):
+    def __init__(self, x, y, health=100):
+        super().__init__(x, y, health)
+        self.character_img = ENEMY_IMG
+        self.mask = pygame.mask.from_surface(self.character_img)
+    def move(self, xvel, yvel):
+        self.x += xvel
+        self.y += yvel
+
 
 class Player(Character):
     def __init__(self, x, y, health=100):
         super().__init__(x, y, health)
-        self.player_img = PLAYER_IMG
+        self.character_img = PLAYER_IMG
         self.arrow_img = ARROW_IMG
-        self.mask = pygame.mask.from_surface(self.player_img)
+        self.mask = pygame.mask.from_surface(self.character_img)
         self.max_health = health
 
 def main():
@@ -51,6 +60,9 @@ def main():
     score = 0
     player_vel = 5
     clock = pygame.time.Clock()
+    enemies = []
+    wave_length = 5
+    enemy_vel = 1
 
     main_font = pygame.font.SysFont("comicsans", 50)
     loss_font = pygame.font.SysFont("comicsans", 60)
@@ -62,6 +74,9 @@ def main():
         lives_label = main_font.render(f"Lives: {lives}", 1, (255, 255, 255))
         score_label = main_font.render(f"Score: {score}", 1, (255, 255, 255))
         level_label = main_font.render(f"Level: {level}", 1, (255, 255, 255))
+
+        for enemy in enemies[:]:
+            enemy.draw(WIN)
 
         player.draw(WIN)
 
@@ -75,6 +90,21 @@ def main():
 
     while run:
         clock.tick(FPS) # Runs at a specific rate regardless of hardware
+
+        # Spawning the enemies in
+        if len(enemies) == 0:
+            level += 1
+            wave_length += 5
+            for i in range(wave_length):
+                xL = random.randrange(-1500, -50)
+                xR = random.randrange(WIDTH+50, WIDTH+1500)
+                yU = random.randrange(-1500, -50)
+                yD = random.randrange( HEIGHT+50, HEIGHT+1500)
+                xRange = [xL, xR]
+                yRange = [yU, yD]
+                enemy = Enemy(random.choice(xRange), random.choice(yRange))
+                enemies.append(enemy)
+
         redraw_window()
         # Exits the window when you press the X button
         for event in pygame.event.get():
@@ -86,8 +116,20 @@ def main():
             player.x -= player_vel
         if keys[pygame.K_w] and player.y - player_vel > 0: # up
             player.y -= player_vel
-        if keys[pygame.K_d] and player.x + player_vel + player.get_width()/3 < WIDTH: # right
+        if keys[pygame.K_d] and player.x + player_vel + player.get_width() < WIDTH: # right
             player.x += player_vel
-        if keys[pygame.K_s] and player.y + player_vel + player.get_height()/3 < HEIGHT: # down
+        if keys[pygame.K_s] and player.y + player_vel + player.get_height() < HEIGHT: # down
             player.y += player_vel
+        
+        # Moving the enemies towards the player
+        for enemy in enemies:
+            if enemy.x >= player.x and enemy.y >= player.y:
+                enemy.move(-enemy_vel, -enemy_vel)
+            if enemy.x >= player.x and enemy.y <= player.y:
+                enemy.move(-enemy_vel, enemy_vel)
+            if enemy.x <= player.x and enemy.y <= player.y:
+                enemy.move(enemy_vel, enemy_vel)
+            if enemy.x <= player.x and enemy.y >= player.y:
+                enemy.move(enemy_vel, -enemy_vel)
+                
 main()
